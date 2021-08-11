0c53b236fa550eb0aeaf86a69c8a0d5ea4e5765a
amitpanthi <apapanthi@gmail.com>
2021-08-11 20:00:12 +0530

    added user_registration.js with standard class, along with a function that validates all its parameters inputs with proper regex, and throws an error on getting invalid input

---
+++ src/user_registration.js
@@ -0,0 +1,61 @@
+class UserRegistration{
+    constructor(fname, lname, email, mobile, password){
+        this.validateRegex(fname, lname, email, mobile, password)
+        this.fname = fname
+        this.lname = lname
+        this.email = email
+        this.mobile = mobile
+        this.password = password
+    }
+
+    validateRegex(...params){
+        let email_rgx = /^([a-zA-Z0-9_])+(@)(\w)+(.)([a-zA-Z]){2,3}(.)?(\w){2,3}?$/
+        let mobile_rgx = /^[\d]{2}[ ][6789][\d]{9}$/
+        let name_rgx = /^([A-Z])([\w]){1}([\w])+$/
+        let password_rgx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]{1}).{8,}$/ //help taken from https://ihateregex.io/expr/password/
+
+        if(!mobile_rgx.test(params[3])){
+            throw("Invalid Mobile Number! Enter a valid mobile number")
+        }
+
+        if(!email_rgx.test(params[2])){
+            throw("Invalid Email ID! Enter a valid email id")
+        }
+
+        if(!name_rgx.test(params[0])){
+            throw("Invalid First Name!")
+        }
+
+        if(!name_rgx.test(params[1])){
+            throw("Invalid First Name!")
+        }
+
+        if(!password_rgx.test(params[4])){
+            throw("Invalid Password!")
+        }
+    }
+}
+
+try{
+    newEmployee = new UserRegistration("Am", "Test", "am23@gmail.co.in", "91 9191919191", "oas123")
+} catch(error){
+    console.log(error)
+}
+
+try{
+    newEmployee = new UserRegistration("Am2", "Test", "am23@gmail.c", "91 9191919191", "oas123")
+} catch(error){
+    console.log(error)
+}
+
+try{
+    newEmployee = new UserRegistration("Am2", "Test", "am23@gmail.com", "91 9191919191", "oas123")
+} catch(error){
+    console.log(error)
+}
+
+try{
+    newEmployee = new UserRegistration("Am2", "Test", "am23@gmail.com", "91 9191919191", "oas123$Z")
+} catch(error){
+    console.log(error)
+}
