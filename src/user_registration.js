3636e62d75066b9c63584d0c469775ca2526bc7c
amitpanthi <apapanthi@gmail.com>
2021-08-13 11:40:15 +0530

    added regex validation of user registration input with the help of promises

--- src/user_registration.js
+++ src/user_registration.js
@@ -1,6 +1,25 @@
 class UserRegistration{
     constructor(fname, lname, email, mobile, password){
-        this.validateRegex(fname, lname, email, mobile, password)
+
+        let email_rgx = /^([a-zA-Z0-9_])+(@)(\w)+(.)([a-zA-Z]){2,3}(.)?(\w){2,3}?$/ 
+        let mobile_rgx = /^[\d]{2}[ ][6789][\d]{9}$/
+        let name_rgx = /^([A-Z])([\w]){1}([\w])+$/
+        let password_rgx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]{1}).{8,}$/ //help taken from https://ihateregex.io/expr/password
+
+        this.validateRegexWithProm(fname, name_rgx).then(
+            () => this.validateRegexWithProm(lname, name_rgx).then(
+                () => this.validateRegexWithProm(mobile, mobile_rgx).then(
+                    () => this.validateRegexWithProm(email, email_rgx).then(
+                        () => this.validateRegexWithProm(password, password_rgx).then(
+                            (result) => console.log(result)
+                        ).catch((err) => console.log(err))
+                    ).catch((err) => console.log(err))
+                ).catch((err) => console.log(err)))
+            .catch((err) => console.log(err)))
+        .catch((err) => console.log(err))
+        
+        // this.validateRegex(fname, lname, email, mobile, password)
+        
         this.fname = fname
         this.lname = lname
         this.email = email
@@ -9,7 +28,9 @@ class UserRegistration{
     }
 
     validateRegex(...params){
-        let email_rgx = /^([a-zA-Z0-9_])+(@)(\w)+(.)([a-zA-Z]){2,3}(.)?(\w){2,3}?$/
+        // name@website.domain.in
+        // name@website.domain
+        let email_rgx = /^([a-zA-Z0-9_])+(@)(\w)+(.)([a-zA-Z]){2,3}(.)?(\w){2,3}?$/ 
         let mobile_rgx = /^[\d]{2}[ ][6789][\d]{9}$/
         let name_rgx = /^([A-Z])([\w]){1}([\w])+$/
         let password_rgx = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]{1}).{8,}$/ //help taken from https://ihateregex.io/expr/password/
@@ -34,6 +55,16 @@ class UserRegistration{
             throw("Invalid Password!")
         }
     }
+
+    validateRegexWithProm(test_str, regex_str){
+        return new Promise((resolve, reject) => {
+            if(regex_str.test(test_str)){
+                resolve(true)
+            } else {
+                reject(`Input ${test_str} is wrong`)
+            }
+        })
+    }
 }
 
 try{
@@ -43,7 +74,7 @@ try{
 }
 
 try{
-    newEmployee = new UserRegistration("Am2", "Test", "am23@gmail.c", "91 9191919191", "oas123")
+    newEmployee = new UserRegistration("Am2", "T", "am23@gmail.c", "91 9191919191", "oas123")
 } catch(error){
     console.log(error)
 }
